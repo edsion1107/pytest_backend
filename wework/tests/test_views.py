9ccb1726575ffc9bf19accfc591550550cbf0190
edsion1107 <zjb861107@gmail.com>
2019-03-06 14:27:04 +0800

    去除allure模块 尝试Travis-ci

--- wework/tests/test_views.py
+++ wework/tests/test_views.py
@@ -8,14 +8,12 @@
 """
 import uuid
 import pytest
-import allure
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APIClient
 from wework.views import random_str
 
 
-@allure.feature("授权")
 @pytest.mark.parametrize("path", ['/wework/oauth2', '/wework/qr_connect'])
 def test_oauth2_redirect(path):
     """直接访问（不带参数时）302跳转"""
@@ -24,16 +22,13 @@ def test_oauth2_redirect(path):
     assert 'https://open.weixin.qq.com' in res.url
 
 
-@allure.feature("授权")
 @pytest.mark.parametrize("path", ['/wework/oauth2', '/wework/qr_connect'])
 def test_oauth2_block(celery_session_app, celery_session_worker, path):
     """模拟非法请求（不带state参数）的情况"""
-    with pytest.raises(AssertionError):
-        APIClient().get(path, data={'code': uuid.uuid4()})
+    assert APIClient().get(path, data={'code': uuid.uuid4()}).status_code == 400
 
 
 @pytest.mark.django_db
-@allure.feature("授权")
 @pytest.mark.parametrize("path", ['/wework/oauth2', '/wework/qr_connect'])
 def test_oauth2_success(celery_session_app, celery_session_worker, path):
     """带参数模拟成功授权的情况"""
@@ -44,7 +39,6 @@ def test_oauth2_success(celery_session_app, celery_session_worker, path):
 
 
 @pytest.mark.django_db
-@allure.feature("授权")
 @pytest.mark.parametrize("path", ['/wework/oauth2', '/wework/qr_connect'])
 def test_oauth2_cancel(celery_session_app, celery_session_worker, path):
     """带参数模拟取消授权的情况"""
@@ -54,7 +48,6 @@ def test_oauth2_cancel(celery_session_app, celery_session_worker, path):
     assert 'oauth_page.html' in [i.name for i in res.templates]
 
 
-@allure.feature("授权")
 @pytest.mark.parametrize("path", ['/wework/oauth2', '/wework/qr_connect'])
 def test_oauth2_http_method(path):
     """测试各种http方法的支持情况"""
@@ -67,7 +60,6 @@ def test_oauth2_http_method(path):
     assert APIClient().patch(path).status_code == 405
 
 
-@allure.feature("通知")
 def test_notify_myself_unauthorized():
     """身份验证未通过"""
     assert APIClient().get('/wework/notification').status_code == 401
@@ -75,7 +67,6 @@ def test_notify_myself_unauthorized():
 
 
 @pytest.mark.django_db
-@allure.feature("通知")
 def test_notify_myself_method_get(celery_session_app):
     """支持GET请求"""
     user = User.objects.create(username=uuid.uuid4(), is_active=False, is_staff=False, is_superuser=False)
@@ -88,7 +79,6 @@ def test_notify_myself_method_get(celery_session_app):
 
 
 @pytest.mark.django_db
-@allure.feature("通知")
 def test_notify_myself_method_post(celery_session_app):
     """支持POST请求"""
     user = User.objects.create(username=uuid.uuid4(), is_active=False, is_staff=False, is_superuser=False)
@@ -108,7 +98,6 @@ def test_notify_myself_method_post(celery_session_app):
 
 
 @pytest.mark.django_db
-@allure.feature("通知")
 def test_notify_myself_bad_request(celery_session_app, celery_session_worker):
     """支持GET请求"""
     user = User.objects.create(username=uuid.uuid4(), is_active=False, is_staff=False, is_superuser=False)
